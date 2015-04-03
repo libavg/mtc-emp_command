8f3ee433c24d58d1555fad01e38df9727ea7d47f
OXullo Intersecans <x@brainrapers.org>
2015-04-03 16:31:46 +0200

    Updated copyright

--- empcommand/__init__.py
+++ empcommand/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # EMP Command: a missile command multitouch clone
-# Copyright (c) 2010-2011 OXullo Intersecans <x@brainrapers.org>. All rights reserved.
+# Copyright (c) 2010-2015 OXullo Intersecans <x@brainrapers.org>. All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without modification, are
 # permitted provided that the following conditions are met:
